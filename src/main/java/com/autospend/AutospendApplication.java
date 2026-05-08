ca225f3f383136241fcc566f1cd5c81c72177e26
shashanksh8e <shashank648a@gmail.com>
2026-05-09 00:40:00 +0530

    Day2 :Spring boot setup, MySQL connected, my first API working

---
+++ src/main/java/com/autospend/AutospendApplication.java
@@ -0,0 +1,13 @@
+package com.autospend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+public class AutospendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutospendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autospend/AutospendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autospend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutospendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
